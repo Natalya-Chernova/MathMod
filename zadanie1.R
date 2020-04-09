8a08f59635372d0091ff213b749848c9240559c4
Natalya-Chernova <62471569+Natalya-Chernova@users.noreply.github.com>
2020-04-09 13:04:41 +0300

    попытка №3

--- zadanie1.R
+++ zadanie1.R
@@ -6,24 +6,38 @@
 library(tidyverse)
 library(rnoaa)
 # Скачаем данные с метеостанций и сохраним результат
+
 station_data = ghcnd_stations() 
+
 write.csv(station_data, file="station_data.csv")
+
 station_data = read.csv("station_data.csv")
+
 #список станций ближайших к столице моего региона,создав таблицу с именем региона и координатами его столицы
+
 simferopol = data.frame(id = "SIMFEROPOL",  latitude = 34.101626,  longitude = 44.955197)
+
 #выбираем количество станций, имеющих необходимые данные за заданный временной период
+
 simferopol_around = meteo_nearby_stations(lat_lon_df = simferopol, station_data = station_data,
                                     limit = 18, var = c("PRCP", "TAVG"),
                                     year_min = 2009, year_max = 2016)
+
 #simferpol_around это список единственным элементом которого является таблица, 
 #содержащая идентификаторы метеостанций отсортированных по их 
 # удаленности от Симферополя, первым элементом таблицы будет идентификатор метеостанции Тулы, 
 #его то мы и попытаемся получить
+
 simferopol_id = simferopol_around[["SIMFEROPOL"]][["id"]][1]
+
 # Получаем данные с одной метеостанции
+
 all_simferopol_data = meteo_tidy_ghcnd(stationid = simferopol_id)
+
 # Смотрим все данные с метеостанции
+
 summary(simferopol_id)
+
 #сюда мы будем загружать данные с метеостанции
 
 all_i=data.frame()
@@ -39,9 +53,13 @@ for(i in 1:18)
 simferopol_id=simferopol_around[["SIMFEROPOL"]][["id"]]
 print(i)
 print(simferopol_id)
-}
-#Зададим интересующие нас даты
 all_i = meteo_tidy_ghcnd(stationid = simferopol_id[i], var="TAVG", date_min = "2009-01-01", date_max = "2016-12-31")
+all_simferopol_meteodata=rbind(all_simferopol_meteodata, all_i)
+}
+# Записываем все результаты
+write.csv(all_simferopol_meteodata, "all_simeropol_meteodata.csv")
+view(all_simferopol_data)
+view(all_simferopol_meteodata)
 
 #Зададим все необходимые для расчёта переменные
 y=1
