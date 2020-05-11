06b2528b5d8bd23c8d3e57a62e1fbe85fcb473e1
Natalya-Chernova <62471569+Natalya-Chernova@users.noreply.github.com>
2020-05-11 09:41:40 +0300

    Add files via upload
    Оно работает, честное слово :)

--- zadanie2.R
+++ zadanie2.R
@@ -90,3 +90,76 @@ anova(mod1)
 #и графики, куда же без них
 plot(mod1)
 # Ура-ура, мы получили кучу графиков!
+
+# А теперь создадим вторую модель, "выкинув как можно больше переменных, которые не вносят почти ничего в качество итоговой модели
+formula2 = h2o_flux ~ (DOY+file_records+Tau+qc_Tau+rand_err_Tau+H+qc_H+rand_err_H+LE+qc_LE+rand_err_LE+co2_flux+qc_co2_flux+rand_err_co2_flux+rand_err_h2o_flux+`co2_v-adv`+`h2o_v-adv`+co2_mole_fraction+co2_mixing_ratio+h2o_molar_density+h2o_mole_fraction+h2o_mixing_ratio+h2o_time_lag+sonic_temperature+air_temperature+air_pressure+air_density+air_heat_capacity+air_molar_volume+water_vapor_density+e+es+specific_humidity+RH+VPD+Tdew+u_unrot+v_unrot+ w_unrot+u_rot +v_rot +w_rot +max_speed+wind_dir+yaw+pitch                  +TKE +L +bowen_ratio +`T*` + x_peak +x_offset +`x_10%`+`x_30%`+`x_50%`+`x_70%`+`x_90%`+un_Tau+Tau_scf+un_H+H_scf+un_LE+LE_scf+un_co2_flux+un_h2o_flux+w_spikes                  +ts_var+h2o_var+`w/ts_cov`+co2)
+mod2 = lm(formula2, data = Teaching_tbl)
+# а теперь узнаем то, что хотим знать о второй модели
+# посмотрим коэффициенты
+coefficients(mod2)
+# посчитаем предсказанные моделью значения
+predict(mod2)
+# потом узнаем остатки
+resid(mod2)
+# и доверительный интервал, конечно же
+confint(mod2, level = 0.95)
+#общие значения по модели будут не лишними
+summary(mod2)
+#дисперсионный анализ - это тоже важно
+anova(mod2)
+#и графики, куда же без них
+plot(mod2)
+# Больше графиков богу графиков :)
+#А теперь мы создадим третью модель, скомпактив вторую
+formula3 = h2o_flux ~ (DOY+file_records+Tau+qc_Tau+rand_err_Tau+H+qc_H+rand_err_H+LE+qc_LE+rand_err_LE+co2_flux+qc_co2_flux+rand_err_co2_flux+rand_err_h2o_flux+`co2_v-adv`+`h2o_v-adv`+co2_mole_fraction+co2_mixing_ratio+h2o_molar_density+h2o_mole_fraction+h2o_mixing_ratio+h2o_time_lag+sonic_temperature+air_temperature+air_pressure+air_density+air_heat_capacity+water_vapor_density+e+es+RH+VPD+Tdew+u_unrot+v_unrot+ w_unrot+u_rot +v_rot+max_speed+wind_dir+yaw+pitch +TKE +L +bowen_ratio +`T*` + x_peak +x_offset +`x_10%`+`x_30%`+`x_50%`+`x_70%`+`x_90%`+un_Tau+Tau_scf+un_H+H_scf+un_LE+LE_scf+un_co2_flux+un_h2o_flux+ts_var)
+mod3 = lm(formula3, data = Teaching_tbl)
+# И повторим всё остальное
+# коэффициенты
+coefficients(mod3)
+# предсказанные моделью значения
+predict(mod3)
+# потом узнаем остатки
+resid(mod3)
+# доверительный интервал
+confint(mod3, level = 0.95)
+#общие значения по модели
+summary(mod3)
+#дисперсионный анализ 
+anova(mod3)
+#и графики
+plot(mod3)
+
+# и скомпактим нашу модель ещё сильнее...
+formula4 = h2o_flux ~ (DOY+file_records+Tau+qc_Tau+rand_err_Tau+H+qc_H+rand_err_H+LE+qc_LE+rand_err_LE+co2_flux+qc_co2_flux+rand_err_co2_flux+rand_err_h2o_flux+`co2_v-adv`+`h2o_v-adv`+co2_mole_fraction+co2_mixing_ratio+h2o_molar_density+h2o_mole_fraction+h2o_mixing_ratio+h2o_time_lag+sonic_temperature+air_temperature+air_pressure+air_density+air_heat_capacity+water_vapor_density+e+es+VPD+u_unrot+v_unrot+u_rot +v_rot+max_speed+wind_dir+yaw+pitch +TKE +L +`T*` + x_peak +x_offset +`x_10%`+`x_30%`+`x_50%`+`x_70%`+un_Tau+Tau_scf+un_H+H_scf+un_LE+LE_scf+un_co2_flux+un_h2o_flux+ts_var)
+mod4 = lm(formula4, data = Teaching_tbl)
+# И повторим всё остальное
+# коэффициенты
+coefficients(mod4)
+# предсказанные моделью значения
+predict(mod4)
+# потом узнаем остатки
+resid(mod4)
+# доверительный интервал
+confint(mod4, level = 0.95)
+#общие значения по модели
+summary(mod4)
+#дисперсионный анализ 
+anova(mod4)
+#и графики
+plot(mod4)
+
+# Так, конечно, можно продолжать очень долго, но можно я закончу скомпакчивание модели?
+# Лучше сделать корреляционный анализ :)
+cor_Teaching_tbl = select(Teaching_tbl, DOY,file_records,Tau,qc_Tau,rand_err_Tau,H,qc_H,rand_err_H,LE,qc_LE,rand_err_LE,co2_flux,qc_co2_flux,rand_err_co2_flux,rand_err_h2o_flux,`co2_v-adv`,`h2o_v-adv`,co2_mole_fraction,co2_mixing_ratio,h2o_molar_density,h2o_mole_fraction,h2o_mixing_ratio,h2o_time_lag,sonic_temperature,air_temperature,air_pressure,air_density,air_heat_capacity,water_vapor_density,e,es,VPD,u_unrot,v_unrot,u_rot,v_rot,max_speed,wind_dir,yaw,pitch,TKE,L,`T*`,x_peak,x_offset,`x_10%`,`x_30%`,`x_50%`,`x_70%`,un_Tau,Tau_scf,un_H,H_scf,un_LE,LE_scf,un_co2_flux,un_h2o_flux,ts_var)
+cor_tbl = cor(cor_Teaching_tbl) %>% as.data.frame
+#Построим графики по полученной модели
+#Построим график h2o_flux от h2o_flux, использовав значения, полученные на модели 4, и на основе обучающей выборки
+qplot(h2o_flux, h2o_flux, data = Teaching_tbl) + geom_line(aes(y = predict(mod4, Teaching_tbl)))
+#Построим график h2o_flux от h2o_flux, использовав значения, полученные на модели 4, и на основе тестирующей выборки
+qplot(h2o_flux, h2o_flux, data = Testing_tbl) + geom_line(aes(y = predict(mod4, Testing_tbl)))
+#Примеры зависимости зависимости от зависимости
+#Больше графиков богу графиков
+qplot(DOY, h2o_flux, data = Testing_tbl) + geom_line(aes(y = predict(mod4, Testing_tbl)))
+qplot(Tau, h2o_flux, data = Testing_tbl) + geom_line(aes(y = predict(mod4, Testing_tbl)))
+qplot(co2_flux, h2o_flux, data = Testing_tbl) + geom_line(aes(y = predict(mod4, Testing_tbl)))
+#Не стреляйте в хим-токсика, он программирует, как умеет
